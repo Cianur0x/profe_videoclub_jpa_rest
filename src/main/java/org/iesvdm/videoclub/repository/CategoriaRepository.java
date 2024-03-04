dfb35cec975ffe6fdf3054159d06370d7eeace36
Michelle <falbqui495@g.educaand.es>
2024-03-04 10:34:08 +0100

    minor change

--- src/main/java/org/iesvdm/videoclub/repository/CategoriaRepository.java
+++ src/main/java/org/iesvdm/videoclub/repository/CategoriaRepository.java
@@ -3,9 +3,10 @@ package org.iesvdm.videoclub.repository;
 import org.iesvdm.videoclub.domain.Categoria;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
     // Métodos creados por JPA
     List<Categoria> findCategoriaByNombreContainingIgnoreCase(String nombre);
